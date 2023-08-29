52306aabc46ff426a4f6b79bdc924a853994fe0b
BNguyen075 <143553789+BNguyen075@users.noreply.github.com>
2023-08-29 15:47:46 -0500

    Create Snoring.py

---
+++ Snoring.py
@@ -0,0 +1,20 @@
+import board
+import digitalio
+import time
+# set up the (red) LED
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+while True:
+    for brightness in [1,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.45,0.4,0.35,0.3,0.25,0.2,0.15,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]:
+        # brightness is measured out of a max of 1.0
+        T_fast = 0.01
+        T_on = brightness*T_fast
+        T_off = (1-brightness)*T_fast
+        num_repeats=10
+        i=0
+        while (i<num_repeats):
+            led.value = True
+            time.sleep(T_on)
+            led.value = False
+            time.sleep(T_off)
+            i=i+1
